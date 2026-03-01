f78ea67c617dec93da63bc5e8ecd3d8c131bb06a
Ton Nom Pro <safa.sedki.etu@univ-lille.fr>
2026-03-01 18:59:48 +0100

    corrections bug , API fonctionne

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,15 +6,42 @@ import "../css/Home.css"
 function Home() {
     const [searchQuery, setSearchQuery] = useState("");
     const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const [movies, setMovies] = useState
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies)
+            } catch (err) {
+                console.log(err)
+                setError("Failed to load movies...")
+            }
+            finally{
+                setLoading(false)
+            }
+        };
+        loadPopularMovies();
+    },[]);
    
-    const handleSearch = (e) => {
-        e.preventDefault();
-         alert(searchQuery);
-         setSearchQuery("");
+    const handleSearch = async (e) => {
+    e.preventDefault();
+    if (!searchQuery.trim()) return
+    if (loading) return
 
+    setLoading(true)
+    try {
+        const searchResults = await searchMovies(searchQuery)
+        setMovies(searchResults)
+        setError(null)
+    } catch (err) {
+        console.log(err)
+        setError("Failed to search movies...")
+    } finally {
+        setLoading(false)
     }
+  };
 
     return ( 
     <div className="home">
@@ -31,13 +58,19 @@ function Home() {
              </button>
         </form>
 
+         {error && <div className="error-message">{error}</div>}
+
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
         <div className="movies-grid">
-            {movies.map(
-                (movie)=>
-            movie.title.toLocaleLowerCase().startsWith(searchQuery) &&  (<MovieCard movie={movie} key={movie.id} /> )
-        )}
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
         </div>
-</div>
-    );
+      )}
+    </div>
+  );
 }
+
 export default Home;
